3fee1b6af30bb073f97c05cc2d4abb6a9d5a0e4a
liuwei <liuw79@gmail.com>
2012-11-27 21:00:11 +0800

    Initial Commit

---
+++ Picker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Picker
+//
+//  Created by LIU WEI on 12-11-27.
+//  Copyright (c) 2012年 LIU WEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
